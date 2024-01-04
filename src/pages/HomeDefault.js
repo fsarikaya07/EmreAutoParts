9eaa8f2ac8b41c69cec94845453fc37b21c01fce
Fatih Sarıkaya <f.sarikaya@gmail.com>
2024-01-05 01:08:18 +0300

    home

--- src/pages/HomeDefault.js
+++ src/pages/HomeDefault.js
@@ -4,14 +4,7 @@ import HeroOne from "../components/hero/HeroOne";
 import AboutFour from '../components/about/AboutFour';
 import ProjectThree from "../components/project/ProjectThree";
 import ServiceThree from "../components/services/ServiceThree";
-import FeatureFour from "../components/features/FeatureFour";
-import FeatureThree from "../components/features/FeatureThree";
-import FaqOne from "../components/faq/FaqOne";
-import TestimonialThree from "../components/testimonial/TestimonialThree";
-import PricingOne from "../components/pricing/PricingOne";
-import WorkPerformanceOne from "../components/work-performance/WorkPerformanceOne";
-import BrandOne from "../components/brand/BrandOne";
-import BlogOne from "../components/blog/BlogOne";
+
 import FooterOne from "../common/footer/FooterOne";
 
 const HomeDefault = () => {
@@ -22,14 +15,6 @@ const HomeDefault = () => {
             <ServiceThree />
             <AboutFour />
             <ProjectThree />
-            {/* <FeatureFour />
-            <FeatureThree />
-            <FaqOne />
-            <TestimonialThree />
-            <PricingOne />
-            <WorkPerformanceOne />
-            <BrandOne />
-            <BlogOne /> */}
             <FooterOne />
         </>
     )
